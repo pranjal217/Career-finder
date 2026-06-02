6bf1ff1b87de3d6cf88c517a1e9fc09cb0883cd0
pranjal <pranjal217@gmail.com>
2026-06-02 22:28:22 +0530

    third commit

--- career-finder/src/assets/pages/About.jsx
+++ career-finder/src/assets/pages/About.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import image from '../tele.png';
+import mission from '../mission.png';
+import notout from '../notout.png';
+import stadium from '../stadium.png';
+import sport from '../sport.png';
+
 
 const About = () => {
   const navigate = useNavigate(); 
@@ -19,7 +24,7 @@ const About = () => {
     <div className='min-h-screen'>
       {/* Page hero */}
       <div className='relative'>
-        <section className='min-h-[80vh]'>
+        <section className='min-h-[87vh]'>
           <h1 className='text-center p-30 md:p-38 uppercase text-[#A1D800] font-judson font-bold text-7xl md:text-9xl'>
             about us
           </h1>
@@ -37,7 +42,7 @@ const About = () => {
             <span className='text-orange-600'>Empowering you</span> to make informed sports career decisions
           </h1>
           <p className='text-xl md:text-2xl text-black mt-6'>
-            Our goal is simple — to make sports career discovery fast, fun, and personal. No lengthy forms, no generic advice. Just a few honest questions and a result that actually means something.
+            Our goal is simple  to make sports career discovery fast, fun, and personal. No lengthy forms, no generic advice. Just a few honest questions and a result that actually means something.
           </p>
           <div className='flex justify-start mt-10'>
             <button
@@ -78,7 +83,96 @@ const About = () => {
   )}
 </div>
 
-      </section>
+  </section>
+
+{/* OUR MISSION */}
+  <section className='min-h-[80vh] '>
+    <div className='flex flex-col md:flex-row min-h-[80vh]'>
+      <div className='flex flex-col  '>
+      <h1 className='text-bold text-5xl md:text-7xl p-8'>
+        <span className='font-judson font-bold  text-white'>OUR </span><br/>
+        <span className='font-luckiest-guy text-[#C3FF0E]'>MISSION</span>
+      </h1>
+      <img src={mission} alt='goal' 
+      className='w-50  md:w-70 '>
+      </img>
+      </div>
+      {/* line */}
+
+      <div className=' hidden md:block w-2 bg-white my-10 '>
+
+      </div>
+
+
+     <p className='text-white text-xl md:text-4xl flex justify-center items-center text-center p-10 tracking-wide leading-relaxed'>
+      We believe every person carries a unique combination of strengths, interests, and drive. In the world of sports, that combination points to something specific  a career that fits like no other. Our quiz cuts 
+      through the noise and shows you exactly where you belong.
+     </p>
+    </div> 
+
+  </section>
+  {/* OUR BELIEF */}
+<section className='grainy min-h-[80vh] bg-bg_green'>
+  <div className='flex flex-col md:flex-row w-full min-h-[80vh]'>
+    
+    {/* LEFT COLUMN - paragraph (wider) */}
+    <div className=' relative w-full md:w-2/3 flex flex-col items-center justify-center '>
+      <p className='text-black z-10 text-xl md:text-4xl p-5 mt-15 text-center tracking-wide leading-relaxed'>
+       SportsCareerFinder was built for anyone who's ever felt lost choosing a sports 
+       career path. We know the feeling the passion is there, but the direction isn't. 
+       That's why we created a smarter way to find where you truly belong in the world of sports.
+
+
+      </p>
+    
+    </div>
+
+    {/* line */}
+    <div className=' relative hidden md:block w-1 bg-black my-5'> </div>
+    {/* RIGHT COLUMN - heading (smaller) */}
+    <div className=' relative w-full md:w-1/4 flex flex-col items-end  p-5'>
+      <h1 className=' font-bold text-5xl md:text-7xl text-center'>
+        <span className='font-judson font-bold text-black'>OUR</span><br/>
+        <span className='font-luckiest-guy text-orange tracking-wider'>BELIEF </span>
+      </h1>
+             <img 
+    src={notout} 
+    alt="stadium"
+    className=' w-50 md:w-90 my-10  '
+  />
+    </div>
+
+  </div>
+</section>
+{/* Our Solution */}
+<section className='min-h-[80vh]'>
+
+<div className='flex flex-col md:flex-row w-full min-h-[80vh]'>
+  {/* header */}
+  <div className='relative flex flex-col w-full md:w-1/3'>
+  <h1 className=' p-8'>
+    <span className='font-bold font-judson text-5xl md:text-7xl text-white'>OUR</span><br/>
+    <span className='font-luckiest-guy text-5xl md:text-7xl  text-blue-700'>SOlUTION</span>
+  </h1>
+  <img src={sport} alt='image' 
+  className='w-60 md:w-80 ml-5 '>
+  </img>
+</div>
+{/* line */}
+<div className='bg-white hidden md:block w-1 my-10'>
+
+</div>
+{/* Para */}
+<div className='flex flex-col justify-center items-center p-5'>
+  <p className='text-xl md:text-4xl text-white text-center leading-relaxed tracking-wide  '>
+    We turned that confusion into a simple quiz that listens to who you are and points you in the right direction. 
+    No guesswork, no generic advice just honest, personalized results built around your strengths, your passion, 
+    and your potential in the world of sports.
+  </p>
+</div>
+
+</div>
+</section>
     </div>
   )
 }
--- career-finder/src/assets/pages/Home.jsx
+++ career-finder/src/assets/pages/Home.jsx
@@ -32,7 +32,7 @@ export default function Home() {
           {/* Begin button */}
           <button
             onClick={() => navigate('/quiz')}
-            className='group flex items-center mt-3 px-6 py-3 bg-[#A1D800] border-3
+            className='group flex items-center mt-3 px-6 py-3 bg-[#A1D800] border-5
              border-black shadow-[5px_5px_0_2px_rgba(0,0,0,1)] active:shadow-none 
              active:translate-x-[6px] hover:scale-105 font-bold transition-all duration-150 '
           >
@@ -48,7 +48,7 @@ export default function Home() {
              {/* Left dark green side */}
           <div className='w-full md:w-1/3  flex flex-col justify-center px-8 md:px-10 py-12 md:py-18'>
 
-            <h2 className='text-3xl md:text-5xl  text-[#A1D800] font-judson font-bold -mt-38'>
+            <h2 className='text-3xl md:text-5xl  text-[#A1D800] font-judson font-bold -mt-50'>
             The <br/> Problem
             </h2>
             <p className='text-[#9CD100] text-base  md:text-lg mt-5 md:mt-10  leading-relaxed '>
@@ -56,7 +56,7 @@ export default function Home() {
             </p>
           </div>
         {/* Right-side */}
-         <div className=' grainy w-full md:w-2/3 flex flex-col justify-center px-8 md:px-12 py-20 md:py-20  bg-[#DFFDBA] '>
+         <div className=' grainy w-full md:w-2/3 flex flex-col justify-center px-8 md:px-12 py-20 md:py-20  bg-[#DFFDBA] -mt-5 '>
          <p className='text-black text-2xl md:text-4xl leading-snug mb-6'>
           Should you coach? Scout? Work in sports media? Sports management? The options are overwhelming — 
           and we're here to simplify it.
@@ -88,7 +88,7 @@ export default function Home() {
           What do we do?
         </h1>
         
-        <p className='relative z-10 px-15 py-10 text-3xl leading-relaxed text-center' >
+        <p className='relative z-10 px-15 py-10 text-3xl leading-relaxed text-center -mt-17' >
           <span className='font-bold italic'>SportsCareerFinder </span>is a smart website designed to help <br/>
           <span className='text-orange-500'> athletes, students 
           and sports enthusiasts</span> discover the career path that best<br/> matches their skills, passion and goals.
